7f5058982ba30ff9f8085609f049d903f2b16115
4rnay <ikuyama_kyo@outlook.jp>
2025-04-22 16:22:07 +0900

    add transient to ServiceCollection, ServiceCollectionProvider

--- src/main/kotlin/net/arnay/amalib/dependency/ServiceCollection.kt
+++ src/main/kotlin/net/arnay/amalib/dependency/ServiceCollection.kt
@@ -4,11 +4,13 @@ import net.arnay.amalib.dependency.impl.ServiceCollectionProvider
 import net.arnay.amalib.shared.Builder
 import kotlin.reflect.KClass
 
+@Suppress("unused")
 class ServiceCollection : Builder<ServiceProvider>
 {
-    val services = mutableMapOf<KClass<*>, Any>()
+    val singletons = mutableMapOf<KClass<*>, Any>()
+    val transients = mutableMapOf<KClass<*>, KClass<*>>()
 
-    inline fun <reified TInterface: Any, reified TImplementation: TInterface> register() : ServiceCollection
+    inline fun <reified TInterface: Any, reified TImplementation: TInterface> addSingleton() : ServiceCollection
     {
         val ctor = TImplementation::class.constructors.firstOrNull() ?:
             throw IllegalArgumentException("Class ${TImplementation::class.simpleName} was not found.")
@@ -16,27 +18,38 @@ class ServiceCollection : Builder<ServiceProvider>
         val args = ctor.parameters.associateWith {
             val dependency = it.type.classifier as? KClass<*> ?:
                 throw IllegalArgumentException("${it.name} was invalid.")
-            services[dependency] ?: throw IllegalArgumentException("${it.name} was not registered.")
+            var res = singletons[dependency]
+            if (res == null)
+               res = transients[dependency] ?: throw IllegalArgumentException("${it.name} was not registered.")
+
+            res
         }
 
         val instance = ctor.callBy(args)
-        services[TInterface::class] = instance
+        singletons[TInterface::class] = instance
 
         return this
     }
 
-    inline fun <reified TInterface: Any, reified TImplementation: TInterface> register(
+    inline fun <reified TInterface: Any, reified TImplementation: TInterface> addSingleton(
         implInstance: () -> TImplementation
     ) : ServiceCollection
     {
-        services[TInterface::class] = implInstance()
+        singletons[TInterface::class] = implInstance()
 
         return this
     }
 
+    inline fun <reified TInterface: Any, reified TImplementation: TInterface> addTransient(): ServiceCollection
+    {
+        transients[TInterface::class] = TImplementation::class
+        return this
+    }
+
+
     override fun build(): ServiceProvider
     {
-        return ServiceCollectionProvider(services)
+        return ServiceCollectionProvider(singletons, transients)
     }
 
 }
--- src/main/kotlin/net/arnay/amalib/dependency/impl/ServiceCollectionProvider.kt
+++ src/main/kotlin/net/arnay/amalib/dependency/impl/ServiceCollectionProvider.kt
@@ -1,20 +1,37 @@
 package net.arnay.amalib.dependency.impl
 
 import net.arnay.amalib.dependency.ServiceProvider
-import org.apache.commons.lang3.ClassUtils.Interfaces
 import kotlin.reflect.KClass
 import kotlin.reflect.cast
 
 class ServiceCollectionProvider(
-    private val services: MutableMap<KClass<*>, Any>
+    private val singletons: MutableMap<KClass<*>, Any>,
+    private val transients: MutableMap<KClass<*>, KClass<*>>
 ) : ServiceProvider
 {
     override fun <TInterface: Any> getRequiredService(clazz: KClass<TInterface>): TInterface
     {
-        val value = services[clazz] ?:
-            throw IllegalArgumentException("Class ${clazz.simpleName} was not registered.")
+        var value = singletons[clazz]
+
+        if (value == null)
+        {
+            val transientType = transients[clazz] ?:
+                throw IllegalArgumentException("Class ${clazz.simpleName} was not registered.")
+            val ctor = transientType.constructors.firstOrNull() ?:
+                throw IllegalArgumentException("Class ${clazz.simpleName} was not found.")
+            val args = ctor.parameters.associateWith {
+                val dependency = it.type.classifier as? KClass<*> ?:
+                throw IllegalArgumentException("${it.name} was invalid.")
+                var res = singletons[dependency]
+                if (res == null)
+                    res = transients[dependency] ?: throw IllegalArgumentException("${it.name} was not registered.")
+
+                res
+            }
+
+            value = ctor.callBy(args)
+        }
 
         return clazz.cast(value)
     }
-
 }
